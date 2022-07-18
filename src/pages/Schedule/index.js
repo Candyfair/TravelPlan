eac2fe02fe9bd99d879a0115260c600631d8dabb
Candy Fair <candice.bastien@gmail.com>
2022-07-18 20:03:09 +0200

    Add main title

--- src/pages/Schedule/index.js
+++ src/pages/Schedule/index.js
@@ -1,9 +1,14 @@
 import './style.scss';
 
+import location from '../../assets/images/location.svg';
+
 const Schedule = () => (
-  <div>
-    Schedule
-  </div>
+  <>
+    <div className="content__header">
+      <img src={location} alt="Location pin" width={24} height={24} className="content__header__img" />
+      <h1 className="content__header__title">Scottish Highlands Super Trip</h1>
+    </div>
+  </>
 );
 
 export default Schedule;
