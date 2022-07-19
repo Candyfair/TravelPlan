3c10fab5a36fa379a966a91564a23c22b873012c
Candy Fair <candice.bastien@gmail.com>
2022-07-19 19:59:29 +0200

    Style Trip component

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
-const Icon = ({ icon, size }) => (
-  <svg width={size} height={size} viewBox="0 0 24 24">
+const Icon = ({ icon, size, viewbox }) => (
+  <svg width={size} height={size} viewBox={viewbox}>
     <path d={icon} />
   </svg>
 );
@@ -9,6 +9,7 @@ const Icon = ({ icon, size }) => (
 Icon.propTypes = {
   icon: PropTypes.string.isRequired,
   size: PropTypes.number.isRequired,
+  viewbox: PropTypes.string.isRequired
 };
 
 export default Icon;
--- src/components/Trips/Trip/index.js
+++ src/components/Trips/Trip/index.js
@@ -6,14 +6,40 @@ import ICONS from '../../../utils/constants';
 const Trip = () => (
   <div className="trip">
 
-    {/* First square */}
-    <div className="trip__first-square">
-      <p className="trip__first-square__date">21st July</p>
-      <Icon icon={ICONS.train} size={27} />
-      <p className="trip__first-square__type">Eurostar</p>
+    {/* First frame */}
+    <div className="trip__frame">
+      <p className="trip__frame__date">21st July</p>
+
+      <div className="trip__frame__transport">
+        <Icon icon={ICONS.train} size={27} viewbox="0 0 24 24" />
+        <p className="trip__frame__transport__name">Eurostar</p>
+      </div>
     </div>
 
-    <p>[Trip]</p>
+    {/* Middle part */}
+    <div className="trip__middle">
+
+      <div className="trip__middle__timetable">
+        <p className="trip__middle__time">15:13</p>
+        <p className="trip__middle__city">Paris</p>
+      </div>
+
+      <div className="trip__middle_arrows">
+        <Icon icon={ICONS.arrows} size={22} viewbox="0 0 14 15" />
+      </div>
+
+      <div className="trip__middle__timetable">
+        <p className="trip__middle__time">16:30</p>
+        <p className="trip__middle__city">London</p>
+      </div>
+    </div>
+
+    {/* Last frame */}
+    <div className="trip__frame details">
+      <p className="trip__frame__details">Coach 10 55 / 56</p>
+
+    </div>
+
   </div>
 );
 
