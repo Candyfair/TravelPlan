58a3a0e8bff4787bc5387d9bcd25f55b6744804f
Candy Fair <candice.bastien@gmail.com>
2022-08-17 14:06:31 +0200

    Add switch captions

--- src/pages/Schedule/index.js
+++ src/pages/Schedule/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable arrow-body-style */
+// Imports
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
@@ -13,13 +15,13 @@ import Steps from '../../components/Steps';
 import Icon from '../../components/Icon';
 import * as CONSTANTS from '../../utils/constants';
 
-// import moment from 'moment';
-
+// == Component
 const Schedule = () => {
   const dispatch = useDispatch();
   const baseURL = process.env.REACT_APP_BASE_URL;
   const { id } = useParams();
 
+  // API call
   useEffect(() => {
     axios.get(`${baseURL}/trips/${id}`)
       .then((res) => {
@@ -32,30 +34,46 @@ const Schedule = () => {
 
   console.log('Je suis dans le composant Schedule');
 
-  // Get trip's period
+  // Get trip's start and end dates
   const { steps } = trip;
   let firstDate = '';
   let lastDate = '';
 
-  if (steps) {
+  const ShowTripDates = () => {
+    return (
+      firstDate !== lastDate
+        ? <h2 className="content__header__subtitle">{moment(firstDate).format('Do MMMM')} - {moment(lastDate).format('Do MMMM YYYY')}</h2>
+        : <h2 className="content__header__subtitle">{moment(lastDate).format('Do MMMM YYYY')}</h2>
+    );
+  };
+
+  const ShowAddStep = () => {
+    return (
+      <h2>Please add a step to your trip by clicking on the button below!</h2>
+    );
+  };
+
+  let caption;
+
+  if (steps && steps.length > 0) {
     firstDate = steps[0].startDate;
     lastDate = steps[steps.length - 1].endDate;
+    caption = <ShowTripDates />;
   }
 
-  return !trip && !steps ? null : (
+  if (steps && steps.length === 0) {
+    caption = <ShowAddStep />;
+  }
+
+  return !trip ? null : (
     <>
       <div className="content__header">
         <h1 className="main-title">{trip.tripName}</h1>
-        {
-          firstDate !== lastDate
-            ? <h2 className="content__header__subtitle">{moment(firstDate).format('Do MMMM')} - {moment(lastDate).format('Do MMMM YYYY')}</h2>
-            : <h2 className="content__header__subtitle">{moment(lastDate).format('Do MMMM YYYY')}</h2>
-
-        }
+        {caption}
       </div>
 
       {
-        trip && <Steps steps={trip.steps} />
+        trip && steps && <Steps steps={trip.steps} />
       }
 
       <button type="button" className="content__addstep">
