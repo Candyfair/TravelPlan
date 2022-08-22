2dc8a74fead39379dfa6e8505888ccc8b6b18fce
Candy Fair <candice.bastien@gmail.com>
2022-08-22 16:34:25 +0200

    Back to previous api headers config

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
-    // 'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    // 'Content-Type': 'application/json',
     // eslint-disable-next-line quote-props
     'Accept': 'application/json',
   },
