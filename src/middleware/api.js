f9126e37aae7a6b32d636a75f5ff9d15a2acd1c2
Candy Fair <candice.bastien@gmail.com>
2022-08-22 17:26:58 +0200

    Back to correct variable

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_UR,
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
     // 'Content-Type': 'application/json',
